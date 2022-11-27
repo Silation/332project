0cf25f4048a59d923201219c2343f4001c6bfc53
gloveman <gloveman@postech.ac.kr>
2022-11-27 23:09:34 +0900

    "Shuffling initial commit"

---
+++ ShufflingTest/src/main/scala/Shuffling.scala
@@ -0,0 +1,13 @@
+object Shuffling{
+  def main(): Unit =
+  {
+    val L1:List[String] = GetData(1)
+    val L2:List[String] = GetData(2)
+    
+  }
+  def GetData(n:Int):List[String]=
+  {
+    //Get Sorted data (Dummy func)
+    List()
+  }
+}
