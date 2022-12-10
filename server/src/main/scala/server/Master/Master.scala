3a1f9b912a0336d0267f555c367d8c399b9a7998
Silation <97874226+Silation@users.noreply.github.com>
2022-12-10 14:48:17 +0900

    Add files via upload

---
+++ server/src/main/scala/server/Master/Master.scala
@@ -0,0 +1,22 @@
+/*package MasterWorker
+
+import scala.concurrent.ExecutionContext
+import MasterWorker.CommonServer
+
+object Master{
+  def main(args: Array[String]): Unit = {
+    assert(args.length >= 1 && args(0).toInt > 0)
+    val workerNum = args(0).toInt
+    val port = {
+      if (args.length == 2) {
+        args(1).toInt
+      } else {
+        50051
+      }
+    }
+    val server = new NetworkServer(ExecutionContext.global, port, workerNum)
+    server.open()
+    server.waitUntilShutdown()
+
+  }
+}*/
