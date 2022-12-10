728b1142982ce204b5f9916d45dc2b527feff2d5
Silation <97874226+Silation@users.noreply.github.com>
2022-12-10 18:22:54 +0900

    update masterServer

--- server/src/main/scala/server/Master/masterServer.scala
+++ server/src/main/scala/server/Master/masterServer.scala
@@ -1,52 +1,20 @@
 // sbt "runMain MasterServer"
-package master
+package server
 
 import io.grpc.ManagedChannelBuilder
-import zio.Console._
-import zio._
 import scala.io.Source
-import scalapb.zio_grpc.{ServerMain, ServiceList, ZManagedChannel}
-
-import zio.Duration._
-import zio.stream.{Stream, ZStream}
-import io.grpc.{Server, ServerBuilder}
+import io.grpc.{Server, ServerBuilder, Status}
 import io.grpc.Status
-import zio.stream.ZSink
-// import scalapb.zio_grpc.Server
-import scalapb.zio_grpc.ServerLayer
-import zio.Console.{print, printLine}
 
-import sortRPC.customSort.{emptySig, connectionSig, SortFinishSig, pivotVal, MergeFinishSig, WorkerNum}
+import sortRPC.customSort._
 
 import scala.io.Source.fromFile
 import java.io._
-
 import java.util.logging.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
 
-
-
-object MasterImpl {
-  var a = -1
-  def WorkerConnection(request: connectionSig): WorkerNum = {
-    a = a + 1
-    new WorkerNum(a)
-  }
-    // ZIO[R with Context, Status, emptySig]
-    // ZIO.succeed(emptySig())
-
-
-  def Sampling(request: SortFinishSig): pivotVal = new pivotVal("string key for each worker")
-    // ZIO[R with Context, Status, pivotVal]
-    // ZIO.succeed(pivotVal( "string key for each worker" ))
-
-  def MergeFinish(request: MergeFinishSig): emptySig = new emptySig
-    // ZIO[R with Context, Status, emptySig]
-    // ZIO.succeed( emptySig( ) )
-}
-
-object MasterServer extends ServerMain {
+object MasterServer{
   private val logger = Logger.getLogger(classOf[MasterServer].getName)
   
   def main(args: Array[String]): Unit = {
@@ -56,41 +24,13 @@ object MasterServer extends ServerMain {
   }
 
   private val port: Int = 50051
-
-  /*
-  def services: ServiceList[Any] = ServiceList.add(MasterImpl)
-
-  def serverWait: ZIO[Any, Throwable, Unit] =
-    for {
-      _ <- printLine("Server is running. Press Ctrl-C to stop.")
-      _ <- (print(".") *> ZIO.sleep(1.second)).forever
-    } yield ()
-
-  def run = myAppLogic.exitCode
-
-  val myAppLogic =
-    serverWait.provideLayer(serverLive(9090))
-    */
-
-  /*
-  val featuresDatabase = JsonFormat.fromJsonString[FeatureDatabase](
-    Source.fromResource("route_guide_db.json").mkString
-  )
-
-  val createRouteGuide = for {
-    routeNotes <- Ref.make(Map.empty[Point, List[RouteNote]])
-  } yield new RouteGuideService(featuresDatabase.feature, routeNotes)
-
-  def services: ServiceList[Any] =
-    ServiceList.addZIO(createRouteGuide)
-  */
 }
 
 class MasterServer(executionContext: ExecutionContext) { self =>
   private[this] var server: Server = null
 
   private def start(): Unit = {
-    server = ServerBuilder.forPort(MasterServer.port).addService(GreeterGrpc.bindService(new MasterImpl, executionContext)).build.start
+    server = ServerBuilder.forPort(MasterServer.port).addService(MWSignalGrpc.bindService(new MasterImpl, executionContext)).build.start
     MasterServer.logger.info("Server started, listening on " + MasterServer.port)
     sys.addShutdownHook {
       System.err.println("*** shutting down gRPC server since JVM is shutting down")
@@ -109,6 +49,34 @@ class MasterServer(executionContext: ExecutionContext) { self =>
     if (server != null) {
       server.awaitTermination()
     }
+  }
+
+  class MasterImpl extends MWSignalGrpc.MWSignal {
+    var a = -1
+    override def WorkerConnection(request: sortRPC.customSort.connectionSig): scala.concurrent.Future[sortRPC.customSort.WorkerNum] = {
+      a = a + 1
+      val reply = sortRPC.customSort.WorkerNum(number = a)
+      Future.successful(reply)
+    }
+    
+    /*: WorkerNum = {
+      a = a + 1
+      new WorkerNum(a)
+    }*/
+
+    override def Sampling(request: SortFinishSig): Future[pivotVal] = {
+      val reply = pivotVal(pivotOfWorker = "string key for each worker")
+      Future.successful(reply)
+    }
+    
+    // : pivotVal = new pivotVal("string key for each worker")
+
+    override def MergeFinish(request: MergeFinishSig): Future[emptySig] = {
+      val reply = emptySig()
+      Future.successful(reply)
+    }
+    
+    // : emptySig = new emptySig
   }
 
 }
